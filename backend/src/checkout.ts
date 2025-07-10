616f75e76d3be577311ee0ccfb6593467e7e7822
Bela Bohlender <bela@bohlender.email>
2025-07-10 21:25:34 +0200

    forward immediatly to success url if already purchased

--- backend/src/checkout.ts
+++ backend/src/checkout.ts
@@ -6,6 +6,7 @@ import {
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { object, string } from "zod/v4";
+import { hasPolarAppBenefit } from "./utils.js";
 
 export function buildCheckoutRoutes(server: FastifyInstance) {
   server.setValidatorCompiler(validatorCompiler);
@@ -20,6 +21,13 @@ export function buildCheckoutRoutes(server: FastifyInstance) {
       const session = await getSession(request);
       if (session == null) {
         return reply.code(401).send("unauthorized");
+      }
+      const hasAppBenefit = await hasPolarAppBenefit(
+        session.user.id,
+        session.user.isAnonymous!
+      );
+      if (hasAppBenefit) {
+        return reply.redirect(request.query.successUrl);
       }
       const { url } = await polarClient.checkouts.create({
         products: [process.env.POLAR_APP_PRO_PRODUCT_ID!],
